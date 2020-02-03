1faad925a8cd5e3233cdcb0557fe365620a0623f
Chrononox <tegbert@gmail.com>
2020-02-03 11:29:02 -0600

    can cycle through robot list to create cards/ also cats now

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 README!
+does cats w/ backgrounds now. The robot invasion has be halted...for now.
--- src/index.js
+++ src/index.js
@@ -3,17 +3,11 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';       //index style sheet
-import Card from './Card'; //structure of the individual card
+import CardList from './CardList';
 import 'tachyons'   //for styling
 import {robots} from './robots'; //must desrtucture as there is no default export
 
-ReactDOM.render(
-    <div>
-    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div>
-    , document.getElementById('root')); 
+ReactDOM.render(<CardList robots={robots} /> , document.getElementById('root')); 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
