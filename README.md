182ccb6fc28c362f2ccee2b11d2cd1901c06bb15
Chrononox <tegbert@gmail.com>
2020-02-03 15:42:14 -0600

    added font, cat backgrounds, json user data

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 README!
+
+
+--updated page title
+--added cat backgrounds
+--added SEGA font
+
 does cats w/ backgrounds now. The robot invasion has be halted...for now.
--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React, { Component } from 'react';
+import './App.css';
 import CardList from './CardList'; //gets all the cards for each user
 import Searchbox from './Searchbox'
-import {robots} from './robots'; //must desrtucture as there is no default export
+// import {robots} from './robots'; //must desrtucture as there is no default export
 
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: '',
         }
     }
 
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {this.setState({robots: users})})
+        
+    }
+
     onSearchChange = (event) =>{
-        this.setState({ searchfield: event.target.value });        
+        this.setState({ searchfield: event.target.value });
     }
 
     render(){
-        const filteredRobots = this.state.robots.filter(robots => {
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-
-        return(
-            <div className='tc'>
-                <h1>catFriends</h1>
-                <Searchbox searchChange={this.onSearchChange} />
-                {<CardList robots={filteredRobots} />}
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return <h1 className='tc'>Loading</h1>
+        }else{
+            return(
+                <div className='tc'>
+                    <h1 className='f1'>catFriends</h1>
+                    <Searchbox searchChange={this.onSearchChange} />
+                    {<CardList robots={filteredRobots} />}
+                </div>
+            )
+        }        
     }
 }
 
