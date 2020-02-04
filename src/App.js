f80d7f84d6639ade37fc5921afb2a8a47e512387
Travis <tegbert@gmail.com>
2020-02-03 22:56:46 -0600

    code n stuff

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ class App extends Component {
         this.state = {
             robots: [],
             searchfield: '',
+            testData: '',
         }
     }
 
@@ -19,27 +20,34 @@ class App extends Component {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => {this.setState({robots: users})})
-        
     }
 
     onSearchChange = (event) =>{
         this.setState({ searchfield: event.target.value });
     }
 
+    onTestData = (event) =>{
+        this.setState({testData: event.target.value});
+        console.log(this.state.testData)
+    }
+
     render(){
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
-            return <h1 className='tc'>Loading...</h1>
-        }else{
+            if (this.state.robots.length === 0) {
+                return <h1 className='tc'>Loading...</h1>
+            }else if (this.state.testData === '0'){
+                return <h1 className='tc f1'>^.^</h1>
+            }else{
             return(
                 <div className='tc'>
                     <h1 className='f1'>catFriends</h1>
-                    <Searchbox searchChange={this.onSearchChange} />
+                    <Searchbox searchChange={this.onSearchChange}/>
                     <Scroll>
                         {<CardList robots={filteredRobots} />}
                     </Scroll>
+                    <Searchbox searchChange={this.onTestData} />
                 </div>
             )
         }        
